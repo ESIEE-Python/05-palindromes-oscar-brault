5548e970e9e4eb7fdc79fd04833f209cb345c6d9
oscar-brault <oscar.brault@edu.esiee.fr>
2024-09-16 13:00:12 +0000

    un message explicatifpalindrome

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 #### Fonction secondaire
-
+"""
+author : oscar.brault@edu.esiee.fr
+"""
 
 def ispalindrome(p):
-
+    """
+    Détermine si une phrase/mot est un palindrome en retournant
+    la phrase ou le mot et vérifiant si elle est identique
+    """
     # votre code ici
-    
+    x = "éêèêë"
+    y = "eeeee"
+    mytable = str.maketrans(x, y)
+    if p.translate(mytable)[::-1] == p.translate(mytable):
+        return True
     return False
 
 #### Fonction principale
@@ -13,7 +22,9 @@ def ispalindrome(p):
 def main():
 
     # vos appels à la fonction secondaire ici
-
+    '''
+    Retourne les résultats de différents mots de test
+    '''
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
 
